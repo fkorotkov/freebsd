varanasisaigithub/freebsd-1

A DIFFUSE feature kernel module which calculates per-flow minimum, mean, maximum and standard deviation interarrival time statistics for use in classification decisions.
Sponsored by:	FreeBSD Foundation
Reviewed by:	bz

sys/netinet/ipfw/diffuse_feature_iat.h
@@ -1,0 +1,59 @@
+/*-
+ * Copyright (c) 2010-2011
+ * 	Swinburne University of Technology, Melbourne, Australia.
+ * All rights reserved.
+ *
+ * This software was developed at the Centre for Advanced Internet
+ * Architectures, Swinburne University of Technology, by Sebastian Zander, made
+ * possible in part by a gift from The Cisco University Research Program Fund, a
+ * corporate advised fund of Silicon Valley Community Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * DIFFUSE packet inter-arrival time feature.
+ */
+
+#ifndef _NETINET_IPFW_DIFFUSE_FEATURE_IAT_H_
+#define _NETINET_IPFW_DIFFUSE_FEATURE_IAT_H_
+
+#define	DI_DEFAULT_IAT_WINDOW 15
+
+/* Main properties, used in kernel and userspace. */
+#define	DI_IAT_NAME "iat"
+#define	DI_IAT_TYPE DI_FEATURE_ALG_UNIDIRECTIONAL
+#define	DI_IAT_NO_STATS 4
+#define	DI_IAT_STAT_NAMES_DECL char *di_iat_stat_names[DI_IAT_NO_STATS]
+#define	DI_IAT_STAT_NAMES DI_IAT_STAT_NAMES_DECL =		\
+{								\
+	"min",							\
+	"mean",							\
+	"max",							\
+	"stdev"							\
+};
+
+struct di_feature_module *iat_module(void);
+
+#endif /* _NETINET_IPFW_DIFFUSE_FEATURE_IAT_H_ */

sys/netinet/ipfw/diffuse_feature_iat_common.h
@@ -1,0 +1,56 @@
+/*-
+ * Copyright (c) 2010-2011
+ * 	Swinburne University of Technology, Melbourne, Australia.
+ * All rights reserved.
+ *
+ * This software was developed at the Centre for Advanced Internet
+ * Architectures, Swinburne University of Technology, by Sebastian Zander, made
+ * possible in part by a gift from The Cisco University Research Program Fund, a
+ * corporate advised fund of Silicon Valley Community Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _NETINET_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_
+#define _NETINET_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_
+
+#define	DI_IAT_TS_ACC_NORMAL	0
+#define	DI_IAT_TS_ACC_ACCURATE	1
+
+struct di_feature_iat_config {
+	struct di_oid	oid;
+
+	/* Sliding/jumping window as number of packets. */
+	int		iat_window;
+	/* Compute stats for partial windows. */
+	int		iat_partial_window;
+	/* Accuracy of timestamp. */
+	int		iat_ts_acc;
+	/* If = 1 we have jumping windows. */
+	int		iat_jump_window;
+	/* Precision in us. */
+	int		iat_prec;
+};
+
+#endif /* _NETINET_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_ */

fkorotkov/freebsd

sys/netpfil/ipfw/diffuse_feature_iat.h
@@ -1,0 +1,59 @@
+/*-
+ * Copyright (c) 2010-2011
+ * 	Swinburne University of Technology, Melbourne, Australia.
+ * All rights reserved.
+ *
+ * This software was developed at the Centre for Advanced Internet
+ * Architectures, Swinburne University of Technology, by Sebastian Zander, made
+ * possible in part by a gift from The Cisco University Research Program Fund, a
+ * corporate advised fund of Silicon Valley Community Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * DIFFUSE packet inter-arrival time feature.
+ */
+
+#ifndef _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_H_
+#define _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_H_
+
+#define	DI_DEFAULT_IAT_WINDOW 15
+
+/* Main properties, used in kernel and userspace. */
+#define	DI_IAT_NAME "iat"
+#define	DI_IAT_TYPE DI_FEATURE_ALG_UNIDIRECTIONAL
+#define	DI_IAT_NO_STATS 4
+#define	DI_IAT_STAT_NAMES_DECL char *di_iat_stat_names[DI_IAT_NO_STATS]
+#define	DI_IAT_STAT_NAMES DI_IAT_STAT_NAMES_DECL =			\
+{									\
+	"min",								\
+	"mean",								\
+	"max",								\
+	"stdev"								\
+};
+
+struct di_feature_module * iat_module(void);
+
+#endif /* _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_H_ */

sys/netpfil/ipfw/diffuse_feature_iat_common.h
@@ -1,0 +1,56 @@
+/*-
+ * Copyright (c) 2010-2011
+ * 	Swinburne University of Technology, Melbourne, Australia.
+ * All rights reserved.
+ *
+ * This software was developed at the Centre for Advanced Internet
+ * Architectures, Swinburne University of Technology, by Sebastian Zander, made
+ * possible in part by a gift from The Cisco University Research Program Fund, a
+ * corporate advised fund of Silicon Valley Community Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_
+#define _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_
+
+#define	DI_IAT_TS_ACC_NORMAL	0
+#define	DI_IAT_TS_ACC_ACCURATE	1
+
+struct di_feature_iat_config {
+	struct di_oid	oid;
+
+	/* Sliding/jumping window as number of packets. */
+	int		iat_window;
+	/* Compute stats for partial windows. */
+	int		iat_partial_window;
+	/* Accuracy of timestamp. */
+	int		iat_ts_acc;
+	/* If = 1 we have jumping windows. */
+	int		iat_jump_window;
+	/* Precision in us. */
+	int		iat_prec;
+};
+
+#endif /* _NETPFIL_IPFW_DIFFUSE_FEATURE_IAT_COMMON_H_ */