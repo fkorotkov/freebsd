varanasisaigithub/freebsd-1

Integrate DIFFUSE related hooks and supporting infrastructure into the IPFW kernel code. DIFFUSE is called both as part of packet processing (for per-flow management, feature calculation and classification) and IPFW related configuration (for handling DIFFUSE related rule configuration). The interface between IPFW and DIFFUSE was intentionally kept hook based to ensure minimal coupling between them. It should therefore be quite simple to integrate DIFFUSE with other firewalls in future if desired.
Sponsored by:	FreeBSD Foundation
Reviewed by:	bz

sys/netinet/ipfw/ip_fw2.c
@@ -73,7 +73,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet/ip_var.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip_fw.h>
-#include <netinet/ipfw/ip_fw_private.h>
+#include <netinet/ip_diffuse.h>
 #include <netinet/ip_carp.h>
 #include <netinet/pim.h>
 #include <netinet/tcp_var.h>
@@ -89,11 +89,17 @@ __FBSDID("$FreeBSD$");
 #include <netinet6/ip6_var.h>
 #endif
 
+#include <netinet/ipfw/diffuse_common.h>
+#include <netinet/ipfw/ip_fw_private.h>
+
 #include <machine/in_cksum.h>	/* XXX for in_cksum */
 
 #ifdef MAC
 #include <security/mac/mac_framework.h>
 #endif
+
+/* DIFFUSE */
+ipfw_ext_t *ipfw_diffuse_ext = NULL;
 
 /*
  * static variables followed by global ones.
@@ -914,6 +920,8 @@ ipfw_chk(struct ip_fw_args *args)
 	 *	MATCH_FORWARD or MATCH_REVERSE otherwise (q != NULL)
 	 */
 	int dyn_dir = MATCH_UNKNOWN;
+	/* For DIFFUSE features. */
+	int flow_dir = MATCH_UNKNOWN;
 	ipfw_dyn_rule *q = NULL;
 	struct ip_fw_chain *chain = &V_layer3_chain;
 
@@ -934,6 +942,12 @@ ipfw_chk(struct ip_fw_args *args)
 	int is_ipv4 = 0;
 
 	int done = 0;		/* flag to exit the outer loop */
+
+	/*
+	 * DIFFUSE data container required for persistent storage between
+	 * separate threads and calls to diffuse_chk_pkt().
+	 */
+	struct di_chk_pkt_args di_args;
 
 	if (m->m_flags & M_SKIP_FIREWALL || (! V_ipfw_vnet_ready))
 		return (IP_FW_PASS);	/* accept */
@@ -1204,6 +1218,17 @@ do {								\
 		IPFW_RUNLOCK(chain);
 		return (IP_FW_PASS);	/* accept */
 	}
+
+	/* Call DIFFUSE before we start matching. */
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+		ipfw_diffuse_ext->chk_pkt_cmd(&di_args, NULL,
+		    O_DI_BEFORE_ALL_RULES, NULL, NULL, args, ulp, pktlen,
+		    &flow_dir, NULL, NULL, &done, &retval);
+
+		if (done)
+			return (retval);
+        }
+
 	if (args->rule.slot) {
 		/*
 		 * Packet has already been tagged as a result of a previous
@@ -1243,6 +1268,7 @@ do {								\
 		uint32_t tablearg = 0;
 		int l, cmdlen, skip_or; /* skip rest of OR block */
 		struct ip_fw *f;
+		int match;
 
 		f = chain->map[f_pos];
 		if (V_set_disable & (1 << f->set) )
@@ -1251,7 +1277,6 @@ do {								\
 		skip_or = 0;
 		for (l = f->cmd_len, cmd = f->cmd ; l > 0 ;
 		    l -= cmdlen, cmd += cmdlen) {
-			int match;
 
 			/*
 			 * check_body is a jump target used when we find a
@@ -2407,7 +2432,22 @@ do {								\
 			}
 
 			default:
+			{
+				int ret;
+
+				if (ipfw_diffuse_ext != NULL &&
+				    ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+					ret = ipfw_diffuse_ext->chk_pkt_cmd(
+					    &di_args, f, cmd->opcode, &cmd,
+					    &cmdlen, args, ulp, pktlen,
+					    &flow_dir, &match, &l, &done,
+					    &retval);
+					if (ret == 0)
+						break;
+				}
+
 				panic("-- unknown opcode %d\n", cmd->opcode);
+			}
 			} /* end of switch() on opcodes */
 			/*
 			 * if we get here with l=0, then match is irrelevant.
@@ -2427,6 +2467,14 @@ do {								\
 		}	/* end of inner loop, scan opcodes */
 #undef PULLUP_LEN
 
+		/* Call DIFFUSE after we have a match for current rule. */
+		if (ipfw_diffuse_ext != NULL &&
+		    ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+			ipfw_diffuse_ext->chk_pkt_cmd(&di_args, f,
+			    O_DI_AFTER_EACH_RULE, NULL, NULL, args, ulp, pktlen,
+			    &flow_dir, &match, NULL, &done, &retval);
+		}
+
 		if (done)
 			break;
 
@@ -2445,6 +2493,14 @@ do {								\
 		printf("ipfw: ouch!, skip past end of rules, denying packet\n");
 	}
 	IPFW_RUNLOCK(chain);
+
+	/* Call DIFFUSE after packet processing. */
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+		ipfw_diffuse_ext->chk_pkt_cmd(&di_args, NULL,
+		    O_DI_AFTER_ALL_RULES, NULL, NULL, args, ulp, pktlen,
+		    &flow_dir, NULL, NULL, &done, &retval);
+	}
+
 #ifdef __FreeBSD__
 	if (ucred_cache != NULL)
 		crfree(ucred_cache);

sys/netinet/ipfw/ip_fw_sockopt.c
@@ -65,6 +65,9 @@ __FBSDID("$FreeBSD$");
 #include <netinet/in.h>
 #include <netinet/ip_var.h> /* hooks */
 #include <netinet/ip_fw.h>
+#include <netinet/ip_diffuse.h>
+
+#include <netinet/ipfw/diffuse_common.h>
 #include <netinet/ipfw/ip_fw_private.h>
 
 #ifdef MAC
@@ -160,7 +163,7 @@ int
 ipfw_add_rule(struct ip_fw_chain *chain, struct ip_fw *input_rule)
 {
 	struct ip_fw *rule;
-	int i, l, insert_before;
+	int i, l, insert_before, ret;
 	struct ip_fw **map;	/* the new array of pointers */
 
 	if (chain->rules == NULL || input_rule->rulenum > IPFW_DEFAULT_RULE-1)
@@ -175,6 +178,16 @@ ipfw_add_rule(struct ip_fw_chain *chain, struct ip_fw *input_rule)
 	if (map == NULL) {
 		free(rule, M_IPFW);
 		return ENOSPC;
+	}
+
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->add_rule != NULL) {
+		ret = ipfw_diffuse_ext->add_rule(input_rule);
+		if (ret) {
+			free(rule, M_IPFW);
+			free(map, M_IPFW);
+			IPFW_UH_WUNLOCK(chain);
+			return (ret);
+		}
 	}
 
 	bcopy(input_rule, rule, l);
@@ -389,6 +402,9 @@ del_entry(struct ip_fw_chain *chain, uint32_t arg)
 			l = RULESIZE(rule);
 			chain->static_len -= l;
 			ipfw_remove_dyn_children(rule);
+			if (ipfw_diffuse_ext != NULL &&
+			    ipfw_diffuse_ext->remove_rule != NULL)
+				ipfw_diffuse_ext->remove_rule(rule);
 			rule->x_next = chain->reap;
 			chain->reap = rule;
 		}
@@ -528,9 +544,10 @@ zero_entry(struct ip_fw_chain *chain, u_int32_t arg, int log_only)
 static int
 check_ipfw_struct(struct ip_fw *rule, int size)
 {
-	int l, cmdlen = 0;
+	struct di_chk_rule_cmd_args di_args;
+	ipfw_insn *cmd, pseudo;
+	int l, ret, cmdlen = 0;
 	int have_action=0;
-	ipfw_insn *cmd;
 
 	if (size < sizeof(*rule)) {
 		printf("ipfw: rule too short\n");
@@ -547,6 +564,14 @@ check_ipfw_struct(struct ip_fw *rule, int size)
 		    rule->act_ofs, rule->cmd_len - 1);
 		return (EINVAL);
 	}
+
+	if (ipfw_diffuse_ext != NULL &&
+	    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+		pseudo.opcode = O_DI_BEFORE_RULE_CHK;
+		pseudo.len = F_INSN_SIZE(ipfw_insn);
+		ipfw_diffuse_ext->chk_rule_cmd(&di_args, &pseudo, &have_action);
+	}
+
 	/*
 	 * Now go for the individual checks. Very simple ones, basically only
 	 * instruction sizes.
@@ -825,12 +850,30 @@ check_action:
 				return EPROTONOSUPPORT;
 #endif
 			default:
+				if (ipfw_diffuse_ext != NULL &&
+				    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+					ret = ipfw_diffuse_ext->chk_rule_cmd(
+					    &di_args, cmd, &have_action);
+					if (ret > 0)
+						return (ret);
+					else if (ret == 0)
+						break;
+				}
+
 				printf("ipfw: opcode %d, unknown opcode\n",
 					cmd->opcode);
 				return EINVAL;
 			}
 		}
 	}
+
+	if (ipfw_diffuse_ext != NULL &&
+	    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+		pseudo.opcode = O_DI_AFTER_RULE_CHK;
+		pseudo.len = F_INSN_SIZE(ipfw_insn);
+		ipfw_diffuse_ext->chk_rule_cmd(&di_args, &pseudo, &have_action);
+	}
+
 	if (have_action == 0) {
 		printf("ipfw: missing action\n");
 		return EINVAL;

fkorotkov/freebsd

sys/netpfil/ipfw/ip_fw2.c
@@ -73,6 +73,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet/ip_var.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip_fw.h>
+#include <netinet/ip_diffuse.h>
 #include <netinet/ip_carp.h>
 #include <netinet/pim.h>
 #include <netinet/tcp_var.h>
@@ -88,6 +89,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet6/ip6_var.h>
 #endif
 
+#include <netpfil/ipfw/diffuse_common.h>
 #include <netpfil/ipfw/ip_fw_private.h>
 
 #include <machine/in_cksum.h>	/* XXX for in_cksum */
@@ -95,6 +97,9 @@ __FBSDID("$FreeBSD$");
 #ifdef MAC
 #include <security/mac/mac_framework.h>
 #endif
+
+/* DIFFUSE */
+ipfw_ext_t *ipfw_diffuse_ext = NULL;
 
 /*
  * static variables followed by global ones.
@@ -958,6 +963,8 @@ ipfw_chk(struct ip_fw_args *args)
 	 *	MATCH_FORWARD or MATCH_REVERSE otherwise (q != NULL)
 	 */
 	int dyn_dir = MATCH_UNKNOWN;
+	/* For DIFFUSE features. */
+	int flow_dir = MATCH_UNKNOWN;
 	ipfw_dyn_rule *q = NULL;
 	struct ip_fw_chain *chain = &V_layer3_chain;
 
@@ -978,6 +985,12 @@ ipfw_chk(struct ip_fw_args *args)
 	int is_ipv4 = 0;
 
 	int done = 0;		/* flag to exit the outer loop */
+
+	/*
+	 * DIFFUSE data container required for persistent storage between
+	 * separate threads and calls to diffuse_chk_pkt().
+	 */
+	struct di_chk_pkt_args di_args;
 
 	if (m->m_flags & M_SKIP_FIREWALL || (! V_ipfw_vnet_ready))
 		return (IP_FW_PASS);	/* accept */
@@ -1248,6 +1261,17 @@ do {								\
 		IPFW_PF_RUNLOCK(chain);
 		return (IP_FW_PASS);	/* accept */
 	}
+
+	/* Call DIFFUSE before we start matching. */
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+		ipfw_diffuse_ext->chk_pkt_cmd(&di_args, NULL,
+		    O_DI_BEFORE_ALL_RULES, NULL, NULL, args, ulp, pktlen,
+		    &flow_dir, NULL, NULL, &done, &retval);
+
+		if (done)
+			return (retval);
+        }
+
 	if (args->rule.slot) {
 		/*
 		 * Packet has already been tagged as a result of a previous
@@ -1287,6 +1311,7 @@ do {								\
 		uint32_t tablearg = 0;
 		int l, cmdlen, skip_or; /* skip rest of OR block */
 		struct ip_fw *f;
+		int match;
 
 		f = chain->map[f_pos];
 		if (V_set_disable & (1 << f->set) )
@@ -1295,7 +1320,6 @@ do {								\
 		skip_or = 0;
 		for (l = f->cmd_len, cmd = f->cmd ; l > 0 ;
 		    l -= cmdlen, cmd += cmdlen) {
-			int match;
 
 			/*
 			 * check_body is a jump target used when we find a
@@ -2480,7 +2504,22 @@ do {								\
 			}
 
 			default:
+			{
+				int ret;
+
+				if (ipfw_diffuse_ext != NULL &&
+				    ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+					ret = ipfw_diffuse_ext->chk_pkt_cmd(
+					    &di_args, f, cmd->opcode, &cmd,
+					    &cmdlen, args, ulp, pktlen,
+					    &flow_dir, &match, &l, &done,
+					    &retval);
+					if (ret == 0)
+						break;
+				}
+
 				panic("-- unknown opcode %d\n", cmd->opcode);
+			}
 			} /* end of switch() on opcodes */
 			/*
 			 * if we get here with l=0, then match is irrelevant.
@@ -2500,6 +2539,14 @@ do {								\
 		}	/* end of inner loop, scan opcodes */
 #undef PULLUP_LEN
 
+		/* Call DIFFUSE after we have a match for current rule. */
+		if (ipfw_diffuse_ext != NULL &&
+		    ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+			ipfw_diffuse_ext->chk_pkt_cmd(&di_args, f,
+			    O_DI_AFTER_EACH_RULE, NULL, NULL, args, ulp, pktlen,
+			    &flow_dir, &match, NULL, &done, &retval);
+		}
+
 		if (done)
 			break;
 
@@ -2516,6 +2563,14 @@ do {								\
 		printf("ipfw: ouch!, skip past end of rules, denying packet\n");
 	}
 	IPFW_PF_RUNLOCK(chain);
+
+	/* Call DIFFUSE after packet processing. */
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->chk_pkt_cmd != NULL) {
+		ipfw_diffuse_ext->chk_pkt_cmd(&di_args, NULL,
+		    O_DI_AFTER_ALL_RULES, NULL, NULL, args, ulp, pktlen,
+		    &flow_dir, NULL, NULL, &done, &retval);
+	}
+
 #ifdef __FreeBSD__
 	if (ucred_cache != NULL)
 		crfree(ucred_cache);

sys/netpfil/ipfw/ip_fw_sockopt.c
@@ -60,7 +60,9 @@ __FBSDID("$FreeBSD$");
 #include <netinet/in.h>
 #include <netinet/ip_var.h> /* hooks */
 #include <netinet/ip_fw.h>
+#include <netinet/ip_diffuse.h>
 
+#include <netpfil/ipfw/diffuse_common.h>
 #include <netpfil/ipfw/ip_fw_private.h>
 
 #ifdef MAC
@@ -156,7 +158,7 @@ int
 ipfw_add_rule(struct ip_fw_chain *chain, struct ip_fw *input_rule)
 {
 	struct ip_fw *rule;
-	int i, l, insert_before;
+	int i, l, insert_before, ret;
 	struct ip_fw **map;	/* the new array of pointers */
 
 	if (chain->rules == NULL || input_rule->rulenum > IPFW_DEFAULT_RULE-1)
@@ -169,6 +171,16 @@ ipfw_add_rule(struct ip_fw_chain *chain, struct ip_fw *input_rule)
 	if (map == NULL) {
 		free(rule, M_IPFW);
 		return ENOSPC;
+	}
+
+	if (ipfw_diffuse_ext != NULL && ipfw_diffuse_ext->add_rule != NULL) {
+		ret = ipfw_diffuse_ext->add_rule(input_rule);
+		if (ret) {
+			free(rule, M_IPFW);
+			free(map, M_IPFW);
+			IPFW_UH_WUNLOCK(chain);
+			return (ret);
+		}
 	}
 
 	bcopy(input_rule, rule, l);
@@ -382,6 +394,9 @@ del_entry(struct ip_fw_chain *chain, uint32_t arg)
 			chain->static_len -= RULESIZE(rule);
 			if (cmd != 1)
 				ipfw_expire_dyn_rules(chain, rule, RESVD_SET);
+			if (ipfw_diffuse_ext != NULL &&
+			    ipfw_diffuse_ext->remove_rule != NULL)
+				ipfw_diffuse_ext->remove_rule(rule);
 			rule->x_next = chain->reap;
 			chain->reap = rule;
 		}
@@ -519,9 +534,10 @@ zero_entry(struct ip_fw_chain *chain, u_int32_t arg, int log_only)
 static int
 check_ipfw_struct(struct ip_fw *rule, int size)
 {
-	int l, cmdlen = 0;
+	struct di_chk_rule_cmd_args di_args;
+	ipfw_insn *cmd, pseudo;
+	int l, ret, cmdlen = 0;
 	int have_action=0;
-	ipfw_insn *cmd;
 
 	if (size < sizeof(*rule)) {
 		printf("ipfw: rule too short\n");
@@ -538,6 +554,14 @@ check_ipfw_struct(struct ip_fw *rule, int size)
 		    rule->act_ofs, rule->cmd_len - 1);
 		return (EINVAL);
 	}
+
+	if (ipfw_diffuse_ext != NULL &&
+	    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+		pseudo.opcode = O_DI_BEFORE_RULE_CHK;
+		pseudo.len = F_INSN_SIZE(ipfw_insn);
+		ipfw_diffuse_ext->chk_rule_cmd(&di_args, &pseudo, &have_action);
+	}
+
 	/*
 	 * Now go for the individual checks. Very simple ones, basically only
 	 * instruction sizes.
@@ -812,12 +836,30 @@ check_action:
 				return EPROTONOSUPPORT;
 #endif
 			default:
+				if (ipfw_diffuse_ext != NULL &&
+				    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+					ret = ipfw_diffuse_ext->chk_rule_cmd(
+					    &di_args, cmd, &have_action);
+					if (ret > 0)
+						return (ret);
+					else if (ret == 0)
+						break;
+				}
+
 				printf("ipfw: opcode %d, unknown opcode\n",
 					cmd->opcode);
 				return EINVAL;
 			}
 		}
 	}
+
+	if (ipfw_diffuse_ext != NULL &&
+	    ipfw_diffuse_ext->chk_rule_cmd != NULL) {
+		pseudo.opcode = O_DI_AFTER_RULE_CHK;
+		pseudo.len = F_INSN_SIZE(ipfw_insn);
+		ipfw_diffuse_ext->chk_rule_cmd(&di_args, &pseudo, &have_action);
+	}
+
 	if (have_action == 0) {
 		printf("ipfw: missing action\n");
 		return EINVAL;