d03e6ea9a5d44ac2ea54b0e5b52330b843ed9a24
Ed Maste <emaste@freebsd.org>
2014-10-24 16:17:10 -0400

    Add build script and src.conf

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export MAKEOBJDIRPREFIX=/tank/emaste/obj
+set -e
 #make buildenv TARGET=arm64
 
 CLEAN="-DNO_CLEAN -DNO_DEPEND"
@@ -8,6 +9,9 @@ MYPATH=$(cd $(dirname $0) && pwd -L)
 SRCCONF="SRCCONF=$MYPATH/src.conf"
 J=-j8
 ARGS="$J $CLEAN $SRCCONF"
+
+#TGT="TARGET_ARCH=aarch64 TARGET=arm64"
+TGT="TARGET=arm64"
 
 build()
 {
@@ -19,17 +23,17 @@ build()
 #    make -i $ARGS buildworld TARGET=arm64
 
     #make -i $ARGS buildworld buildkernel TARGET=arm64
-    make $ARGS buildworld buildkernel TARGET=arm64
-#    make $ARGS buildkernel TARGET=arm64
+    make $ARGS buildworld $TGT
+    make $ARGS buildkernel $TGT
 
     ls -l $MAKEOBJDIRPREFIX/arm64.arm64/tank/emaste/src/freebsd-arm64/sys/GENERIC/kernel*
 }
 
 build_kernel()
 {
-    make $ARGS kernel-toolchain TARGET=arm64
-    make $ARGS _includes TARGET=arm64
-    make $ARGS buildkernel MODULES_OVERRIDE= TARGET=arm64
+    make $ARGS kernel-toolchain $TGT
+    make $ARGS _includes $TGT
+    make $ARGS buildkernel MODULES_OVERRIDE= $TGT
 }
 
 build 2>&1 | tee build.log
