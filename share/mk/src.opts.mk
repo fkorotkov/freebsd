d05685c6a610656f36aec304b81851bab7180788
Ed Maste <emaste@freebsd.org>
2015-12-29 12:56:58 -0500

    Move GPL_DTC back to default NO option
    As of r292876 the BSD licensed dtc(1) is functional.
    
    Next steps:	Confirm above statement is true.

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -98,7 +98,6 @@ __DEFAULT_YES_OPTIONS = \
     GNU \
     GNU_GREP_COMPAT \
     GPIO \
-    GPL_DTC \
     GROFF \
     HAST \
     HTML \
@@ -180,6 +179,7 @@ __DEFAULT_NO_OPTIONS = \
     CLANG_EXTRAS \
     DTRACE_TESTS \
     EISA \
+    GPL_DTC \
     HESIOD \
     LIBSOFT \
     NAND \
