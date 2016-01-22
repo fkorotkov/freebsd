245e41164c19644084f0efe4ce428d4cbc062095
Ed Maste <emaste@freebsd.org>
2016-01-22 17:08:17 -0500

    Add an assert to track down potential universe build failure

--- contrib/elftoolchain/elfcopy/symbols.c
+++ contrib/elftoolchain/elfcopy/symbols.c
@@ -25,6 +25,7 @@
  */
 
 #include <sys/param.h>
+#include <assert.h>
 #include <err.h>
 #include <fnmatch.h>
 #include <stdio.h>
@@ -584,8 +585,10 @@ generate_symbols(struct elfcopy *ecp)
 		 * If the symbol is a STT_SECTION symbol, mark the section
 		 * it points to.
 		 */
-		if (GELF_ST_TYPE(sym.st_info) == STT_SECTION)
+		if (GELF_ST_TYPE(sym.st_info) == STT_SECTION) {
+			assert(ecp->secndx[sym.st_shndx] < (uint64_t)ecp->nos);
 			BIT_SET(ecp->v_secsym, ecp->secndx[sym.st_shndx]);
+		}
 	}
 
 	/*
