8f10731a19a02d6aff072f54141408ca81b2014b
Ed Maste <emaste@freebsd.org>
2016-07-05 15:15:31 -0400

    Enable LLVM libunwind on arm

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -242,8 +242,8 @@ BROKEN_OPTIONS+=PROFILE # "sorry, unimplemented: profiler support for RISC-V"
 BROKEN_OPTIONS+=TESTS   # "undefined reference to `_Unwind_Resume'"
 BROKEN_OPTIONS+=CXX     # "libcxxrt.so: undefined reference to `_Unwind_Resume_or_Rethrow'"
 .endif
-.if ${__T} == "aarch64" || ${__T} == "amd64" || ${__T} == "i386" || \
-    ${__T} == "riscv64"
+.if ${__T} == "aarch64" || ${__T} == "amd64" || ${__TT} == "arm" || \
+    ${__T} == "i386" || ${__T} == "riscv64"
 __DEFAULT_YES_OPTIONS+=LLVM_LIBUNWIND
 .else
 __DEFAULT_NO_OPTIONS+=LLVM_LIBUNWIND
