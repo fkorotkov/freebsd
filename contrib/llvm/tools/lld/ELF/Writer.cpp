65ba1ca50fe3a1072ae40f1e7334988978a48ef2
Ed Maste <emaste@freebsd.org>
2018-01-25 17:01:10 -0500

    lld: Multi-GOT implementation
    Almost all entries inside MIPS GOT are referenced by signed 16-bit
    index.  Zero entry lies approximately in the middle of the GOT.  So the
    total number of GOT entries cannot exceed ~16384 for 32-bit architecture
    and ~8192 for 64-bit architecture.  This limitation makes impossible to
    link rather large application like for example LLVM+Clang.  There are
    two workaround for this problem.  The first one is using the -mxgot
    compiler's flag.  It enables using a 32-bit index to access GOT entries.
    But each access requires two assembly instructions two load GOT entry
    index to a register.  Another workaround is multi-GOT.  This patch
    implements it.
    
    Here is a brief description of multi-GOT for detailed one see the
    following link https://dmz-portal.mips.com/wiki/MIPS_Multi_GOT.
    
    If the sum of local, global and tls entries is less than 64K only single
    got is enough.  Otherwise, multi-got is created.  Series of primary and
    multiple secondary GOTs have the following layout:
    
    - Primary GOT
        Header
        Local entries
        Global entries
        Relocation only entries
        TLS entries
    
    - Secondary GOT
        Local entries
        Global entries
        TLS entries
    ...
    
    All GOT entries required by relocations from a single input file
    entirely belong to either primary or one of secondary GOTs.  To
    reference GOT entries each GOT has its own _gp value points to the
    "middle" of the GOT.  In the code this value loaded to the register
    which is used for GOT access.
    
    MIPS 32 function's prologue:
    
    lui     v0,0x0
    0: R_MIPS_HI16  _gp_disp
    addiu   v0,v0,0
    4: R_MIPS_LO16  _gp_disp
    
    MIPS 64 function's prologue:
    
    lui     at,0x0
    14: R_MIPS_GPREL16  main
    
    Dynamic linker does not know anything about secondary GOTs and cannot
    use a regular MIPS mechanism for GOT entries initialization.  So we have
    to use an approach accepted by other architectures and create dynamic
    relocations R_MIPS_REL32 to initialize global entries (and local in case
    of PIC code) in secondary GOTs.  But ironically MIPS dynamic linker
    requires GOT entries and correspondingly ordered dynamic symbol table
    entries to deal with dynamic relocations.  To handle this problem
    relocation-only section in the primary GOT contains entries for all
    symbols referenced in global parts of secondary GOTs.  Although the sum
    of local and normal global entries of the primary got should be less
    than 64K, the size of the primary got (including relocation-only
    entries) can be greater than 64K, because parts of the primary got that
    overflow the 64K limit are used only by the dynamic linker at dynamic
    link-time and not by 16-bit gp-relative addressing at run-time.
    
    The patch affects common LLD code in the following places:
    
    * Added new hidden -mips-got-size flag.  This flag required to set low
      maximum size of a single GOT to be able to test the implementation
      using small test cases.
    
    * Added InputFile argument to the getRelocTargetVA function. The same
      symbol referenced by GOT relocation from different input file might be
      allocated in different GOT.  So result of relocation depends on the
      file.
    
    * Added new ctor to the DynamicReloc class.  This constructor records
      settings of dynamic relocation which used to adjust address of 64kb
      page lies inside a specific output section.
    
    With the patch LLD is able to link all LLVM+Clang+LLD applications and
    libraries for MIPS 32/64 targets.
    
    Obtained from:	LLVM review D31528 by Simon Atanasyan

--- contrib/llvm/tools/lld/ELF/Writer.cpp
+++ contrib/llvm/tools/lld/ELF/Writer.cpp
@@ -1433,6 +1433,9 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
   if (errorCount())
     return;
 
+  if (InX::MipsGot)
+    InX::MipsGot->build<ELFT>();
+
   removeUnusedSyntheticSections();
 
   sortSections();
