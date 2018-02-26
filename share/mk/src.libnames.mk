d013c02b91ab34e0fcb8d46859ec87a924786818
Ed Maste <emaste@freebsd.org>
2018-02-25 19:17:29 -0500

    Introduce cap_fileargs.
    Submitted by:	oshogbo
    Differential Revision:	https://reviews.freebsd.org/D14407

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -73,6 +73,7 @@ _LIBRARIES=	\
 		cam \
 		casper \
 		cap_dns \
+		cap_fileargs \
 		cap_grp \
 		cap_pwd \
 		cap_random \
@@ -233,6 +234,7 @@ _DP_cam=	sbuf
 _DP_kvm=	elf
 _DP_casper=	nv
 _DP_cap_dns=	nv
+_DP_cap_fileargs=	nv
 _DP_cap_grp=	nv
 _DP_cap_pwd=	nv
 _DP_cap_random=	nv
