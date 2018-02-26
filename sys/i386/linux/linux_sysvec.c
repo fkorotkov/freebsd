3d47a433fe432e51edf1cbbbca3701bf89b088c3
Ed Maste <emaste@freebsd.org>
2018-02-26 15:04:29 -0500

    Remove stray ; from linux_vdso_deinstall function definition
    Sponsored by:	Turing Robotic Industries Inc.

--- sys/i386/linux/linux_sysvec.c
+++ sys/i386/linux/linux_sysvec.c
@@ -1037,7 +1037,7 @@ linux_vdso_deinstall(const void *param)
 {
 
 	__elfN(linux_shared_page_fini)(linux_shared_page_obj);
-};
+}
 SYSUNINIT(elf_linux_vdso_uninit, SI_SUB_EXEC, SI_ORDER_FIRST,
     linux_vdso_deinstall, NULL);
 
