330e44719e8c711ce1337258ad420cdd5c6d810c
Michal Stanek <mst@semihalf.com>
2018-04-03 20:06:30 +0200

    Implement pmap_mapbios and pmap_unmapbios to fit new DMAP with L2 resolution

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -217,6 +217,21 @@ __FBSDID("$FreeBSD$");
 
 struct pmap kernel_pmap_store;
 
+/* Used for mapping ACPI memory before VM is initialized */
+#define	PMAP_PREINIT_MAPPING_COUNT	8
+#define PMAP_PREINIT_MAPPING_SIZE	(PMAP_PREINIT_MAPPING_COUNT * L2_SIZE)
+static vm_offset_t preinit_map_va;	/* Start VA of pre-init mapping space */
+static int vm_initialized = 0;		/* No need to use pre-init maps when set */
+
+/*
+ * Reserve a few L2 blocks starting from 'preinit_map_va' pointer.
+ * Always map entire L2 block for simplicity.
+ * VA = preinit_mapv + i * L2_SIZE
+ */
+static struct pmap_preinit_mapping {
+	vm_paddr_t	pa;	/* Aligned to L2_SIZE */
+} pmap_preinit_mapping[PMAP_PREINIT_MAPPING_COUNT];
+
 vm_offset_t virtual_avail;	/* VA of first avail page (after kernel bss) */
 vm_offset_t virtual_end;	/* VA of last avail page (end of kernel AS) */
 vm_offset_t kernel_vm_end = 0;
@@ -788,7 +803,11 @@ pmap_bootstrap(vm_offset_t l0pt, vm_offset_t l1pt, vm_paddr_t kernstart,
 	alloc_pages(msgbufpv, round_page(msgbufsize) / PAGE_SIZE);
 	msgbufp = (void *)msgbufpv;
 
-	virtual_avail = roundup2(freemempos, L1_SIZE);
+	/* Reserve some VA space for early BIOS/ACPI mapping */
+	preinit_map_va = roundup2(freemempos, L2_SIZE);
+
+	virtual_avail = preinit_map_va + PMAP_PREINIT_MAPPING_SIZE;
+	virtual_avail = roundup2(virtual_avail, L1_SIZE);
 	virtual_end = VM_MAX_KERNEL_ADDRESS - L2_SIZE;
 	kernel_vm_end = virtual_avail;
 
@@ -884,6 +903,8 @@ pmap_init(void)
 	for (i = 0; i < pv_npg; i++)
 		TAILQ_INIT(&pv_table[i].pv_list);
 	TAILQ_INIT(&pv_dummy.pv_list);
+
+	vm_initialized = 1;
 }
 
 static SYSCTL_NODE(_vm_pmap, OID_AUTO, l2, CTLFLAG_RD, 0,
@@ -4251,13 +4272,114 @@ pmap_clear_modify(vm_page_t m)
 void *
 pmap_mapbios(vm_paddr_t pa, vm_size_t size)
 {
+	struct pmap_preinit_mapping *ppim;
+	vm_offset_t va, offset;
+	pd_entry_t *pde;
+	pt_entry_t *l2;
+	int i, lvl;
 
-        return ((void *)PHYS_TO_DMAP(pa));
+	if (!vm_initialized) {
+		if (size > L2_SIZE) {
+			if (bootverbose)
+				printf("%s: mapping size=%lx too big!\n",
+				    __func__, size);
+			return (NULL);
+		}
+		if ((pa & L2_OFFSET) != 0 &&
+			size > (roundup2(pa, L2_SIZE) - pa)) {
+			if (bootverbose)
+				printf("%s: mapping pa=%lx size=%lx crosses L2 boundary!\n",
+				    __func__, pa, size);
+			return (NULL);
+		}
+
+		/* Map 2MiB L2 blocks from reserved VA space */
+		offset = pa & L2_OFFSET;
+		pa = rounddown2(pa, L2_SIZE);
+
+		/* If we have a preinit mapping, re-use it */
+		for (i = 0; i < PMAP_PREINIT_MAPPING_COUNT; i++) {
+			ppim = pmap_preinit_mapping + i;
+			if (ppim->pa == pa) {
+				va = preinit_map_va + (i * L2_SIZE);
+				return ((void *)(va + offset));
+			}
+		}
+		if (preinit_map_va == 0)
+			return (NULL);
+
+		va = 0;
+		for (i = 0; i < PMAP_PREINIT_MAPPING_COUNT; i++) {
+			ppim = pmap_preinit_mapping + i;
+			if (ppim->pa == 0) {
+				ppim->pa = pa;
+				va = preinit_map_va + (i * L2_SIZE);
+				break;
+			}
+		}
+		if (va == 0)
+			panic("%s: too many preinit mappings", __func__);
+
+		pde = pmap_pde(kernel_pmap, va, &lvl);
+		KASSERT(pde != NULL,
+		    ("pmap_mapbios: Invalid page entry, va: 0x%lx", va));
+		KASSERT(lvl == 1, ("pmap_mapbios: Invalid level %d", lvl));
+
+		/* Insert L2_BLOCK */
+		l2 = pmap_l1_to_l2(pde, va);
+		pmap_load_store(l2,
+		    pa | ATTR_DEFAULT | ATTR_XN |
+		    ATTR_IDX(CACHED_MEMORY) | L2_BLOCK);
+		pmap_invalidate_range(kernel_pmap, va, va + L2_SIZE);
+
+	} else {
+		/* kva_alloc may be used to map the pages */
+		offset = pa & PAGE_MASK;
+		size = round_page(offset + size);
+
+		va = kva_alloc(size);
+		if (va == 0)
+			panic("%s: Couldn't allocate KVA", __func__);
+
+		pde = pmap_pde(kernel_pmap, va, &lvl);
+		KASSERT(lvl == 2, ("pmap_mapbios: Invalid level %d", lvl));
+
+		/* L3 table is linked */
+		va = trunc_page(va);
+		pa = trunc_page(pa);
+		pmap_kenter(va, size, pa, CACHED_MEMORY);
+	}
+
+	return ((void *)(va + offset));
 }
 
 void
-pmap_unmapbios(vm_paddr_t pa, vm_size_t size)
+pmap_unmapbios(vm_offset_t va, vm_size_t size)
 {
+	vm_offset_t offset, tmpsize;
+	pd_entry_t *pde;
+	int lvl;
+
+	/*
+	 * No need to unmap early L2 block mappings.
+	 * Only unmap the pages reserved with kva_alloc.
+	 */
+	if (vm_initialized) {
+		offset = va & PAGE_MASK;
+		size = round_page(offset + size);
+		va = trunc_page(va);
+
+		kva_free(va, size);
+
+		pde = pmap_pde(kernel_pmap, va, &lvl);
+		KASSERT(pde != NULL,
+		    ("pmap_unmapbios: Invalid page entry, va: 0x%lx", va));
+		KASSERT(lvl == 2, ("pmap_unmapbios: Invalid level %d", lvl));
+
+		/* Unmap and invalidate the pages */
+                for (tmpsize = 0; tmpsize < size; tmpsize += PAGE_SIZE)
+			pmap_kremove(va + tmpsize);
+	}
 }
 
 /*
