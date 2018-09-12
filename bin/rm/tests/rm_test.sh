fkorotkov/freebsd

commit 869660fff34d23bd09a7be023a2c3f009348541d

Reject options, handle "--" correctly in unlink(1).
git-svn-id: https://svn.freebsd.org/base/head@97533 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

bin/rm/rm.c
@@ -95,11 +95,14 @@ main(int argc, char *argv[])
 	else
 		++p;
 	if (strcmp(p, "unlink") == 0) {
-		if (argc == 2) {
-			rm_file(&argv[1]);
-			exit(eval);
-		} else 
+		while (getopt(argc, argv, "") != -1)
 			usage();
+		argc -= optind;
+		argv += optind;
+		if (argc == 0)
+			usage();
+		rm_file(&argv[0]);
+		exit(eval);
 	}
 
 	Pflag = rflag = 0;

commit a8306f5a5b76d7dee7fc1801dddfeed26aae559f

Fix unlink(1) for files starting with -
Restore the original behavior of unlink(1), passing the provided filename
directly to unlink(2), handling the first argument being "--" correctly.

This fixes "unlink -foo", broken in r97533.

PR:		228448
Reported by:	Brennan Vincent <brennan@umanwizard.com>
Submitted by:	Brennan Vincent <brennan@umanwizard.com> (original version)
Submitted by:	Yuri Pankov
Reviewed by:	emaste, kevans, vangyzen

bin/rm/Makefile
@@ -1,10 +1,15 @@
 #	@(#)Makefile	8.1 (Berkeley) 5/31/93
 # $FreeBSD$
 
+.include <src.opts.mk>
+
 PACKAGE=runtime
 PROG=	rm
 
 LINKS=	${BINDIR}/rm ${BINDIR}/unlink
 MLINKS=	rm.1 unlink.1
+
+HAS_TESTS=
+SUBDIR.${MK_TESTS}+= tests
 
 .include <bsd.prog.mk>

bin/rm/rm.1
@@ -32,7 +32,7 @@
 .\"	@(#)rm.1	8.5 (Berkeley) 12/5/94
 .\" $FreeBSD$
 .\"
-.Dd November 7, 2015
+.Dd September 12, 2018
 .Dt RM 1
 .Os
 .Sh NAME
@@ -45,6 +45,7 @@
 .Op Fl dIPRrvWx
 .Ar
 .Nm unlink
+.Op Fl -
 .Ar file
 .Sh DESCRIPTION
 The
@@ -154,6 +155,9 @@ No options may be supplied in this simple mode of operation,
 which performs an
 .Xr unlink 2
 operation on the passed argument.
+However, the usual option-end delimiter,
+.Fl - ,
+may optionally precede the argument.
 .Sh EXIT STATUS
 The
 .Nm
@@ -201,11 +205,12 @@ directory hierarchy:
 .Pp
 .Dl $ rm -rf foobar
 .Pp
-Either of these commands will remove the file
+Any of these commands will remove the file
 .Pa -f :
 .Bd -literal -offset indent
 $ rm -- -f
 $ rm ./-f
+$ unlink -f
 .Ed
 .Sh COMPATIBILITY
 The

bin/rm/rm.c
@@ -101,13 +101,12 @@ main(int argc, char *argv[])
 	else
 		++p;
 	if (strcmp(p, "unlink") == 0) {
-		while (getopt(argc, argv, "") != -1)
+		if (argc == 2)
+			rm_file(&argv[1]);
+		else if (argc == 3 && strcmp(argv[1], "--") == 0)
+			rm_file(&argv[2]);
+		else
 			usage();
-		argc -= optind;
-		argv += optind;
-		if (argc != 1)
-			usage();
-		rm_file(&argv[0]);
 		exit(eval);
 	}
 
@@ -634,7 +633,7 @@ usage(void)
 
 	(void)fprintf(stderr, "%s\n%s\n",
 	    "usage: rm [-f | -i] [-dIPRrvWx] file ...",
-	    "       unlink file");
+	    "       unlink [--] file");
 	exit(EX_USAGE);
 }
 

bin/rm/tests/Makefile
@@ -1,0 +1,5 @@
+# $FreeBSD$
+
+ATF_TESTS_SH+=  rm_test
+
+.include <bsd.test.mk>

bin/rm/tests/Makefile.depend
@@ -1,0 +1,11 @@
+# $FreeBSD$
+# Autogenerated - do NOT edit!
+
+DIRDEPS = \
+
+
+.include <dirdeps.mk>
+
+.if ${DEP_RELDIR} == ${_DEP_RELDIR}
+# local dependencies - needed for -jN in clean tree
+.endif

bin/rm/tests/rm_test.sh
@@ -1,0 +1,45 @@
+#
+# Copyright 2018 Yuri Pankov
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $FreeBSD$
+#
+
+atf_test_case unlink_dash_filename
+unlink_dash_filename_head()
+{
+	atf_set "descr" "unlink correctly handles -filename"
+}
+unlink_dash_filename_body()
+{
+	touch -- foo bar -foo -bar
+	atf_check -s exit:0 unlink foo
+	atf_check -s exit:0 unlink -- bar
+	atf_check -s exit:0 unlink -foo
+	atf_check -s exit:0 unlink -- -bar
+}
+
+atf_init_test_cases()
+{
+	atf_add_test_case unlink_dash_filename
+}

etc/mtree/BSD.tests.dist
@@ -34,6 +34,8 @@
         ..
         pwait
         ..
+        rm
+        ..
         rmdir
         ..
         sh