d356ba7f546fea3ff412ffb05e31eb8931fd26f8
Ed Maste <emaste@freebsd.org>
2018-09-19 10:29:41 -0400

    Revert "clang: Enable IAS for FreeBSD/mips*"
    An equivalent change has been made upstream, so revert ours to avoid a
    conflict upon merge.
    
    This reverts commit a22f53d80f7e10ccdd77fd47090a4df1cfc91c5d.

--- contrib/llvm/tools/clang/lib/Driver/ToolChains/Gnu.cpp
+++ contrib/llvm/tools/clang/lib/Driver/ToolChains/Gnu.cpp
@@ -2249,10 +2249,8 @@ bool Generic_GCC::IsIntegratedAssemblerDefault() const {
     // Enabled for Debian and Android mips64/mipsel, as they can precisely
     // identify the ABI in use (Debian) or only use N64 for MIPS64 (Android).
     // Other targets are unable to distinguish N32 from N64.
-    // XXX Enabled for FreeBSD for testing; if N32 IAS does not work we will
-    // just skip the architecture.
     if (getTriple().getEnvironment() == llvm::Triple::GNUABI64 ||
-        getTriple().isAndroid() || getTriple().getOS() == llvm::Triple::FreeBSD)
+        getTriple().isAndroid())
       return true;
     return false;
   default:
