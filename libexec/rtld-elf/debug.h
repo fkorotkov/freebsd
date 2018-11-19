66e0100f700527e88084dfed51788f7db88d547f
Ed Maste <emaste@freebsd.org>
2018-11-19 08:52:07 -0500

    rtld: enable DEBUG universally

--- libexec/rtld-elf/debug.h
+++ libexec/rtld-elf/debug.h
@@ -42,9 +42,9 @@
 extern void debug_printf(const char *, ...) __printflike(1, 2);
 extern int debug;
 
-// Binaries using lld-linked i386 libc crash on startup so compile debug in
-// by default there.
-#if !defined(DEBUG) && defined(__i386__)
+// Enable DEBUG by default as we experiment with tool chain changes which may
+// break rtld.
+#if !defined(DEBUG)
 #define DEBUG
 #endif
 
